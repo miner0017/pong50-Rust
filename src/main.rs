c54eda7cf13feae2efdbc32e6a8fab22b363ec31
miner0017 <miner.wk51@gmail.com>
2021-12-24 16:41:08 -0600

    Changed the text to be built using the UI Camera Adjusted size of ball and text scales

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,38 @@ fn setup(
         commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
         // 2D Text 
-        commands.spawn_bundle(Text2dBundle {
-            text: Text::with_section(
-                "Hello, World!",
-                TextStyle {
-                    font: asset_server.load("fonts/font.ttf"),
-                    font_size: 60.0,
-                    color: Color::WHITE,
+        // UI camera
+        commands.spawn_bundle(UiCameraBundle::default());
+        // Text with one section
+        commands
+            .spawn_bundle(TextBundle {
+                style: Style {
+                    align_self: AlignSelf::FlexEnd,
+                    position_type: PositionType::Relative,
+                    position: Rect {
+                        top: Val::Px(0.0),
+                        right: Val::Px(0.0),
+                        ..Default::default()
+                    },
+                    ..Default::default()
                 },
-                TextAlignment {
-                    vertical: VerticalAlign::Center,
-                    horizontal: HorizontalAlign::Center,
-                },
-            ),
-            ..Default::default()
-        });
+                // Use the `Text::with_section` constructor
+                text: Text::with_section(
+                    // Accepts a `String` or any type that converts into a `String`, such as `&str`
+                    "Hello, Pong!",
+                    TextStyle {
+                        font: asset_server.load("fonts/font.ttf"),
+                        font_size: 25.0,
+                        color: Color::WHITE,
+                    },
+                    // Note: You can use `Default::default()` in place of the `TextAlignment`
+                    TextAlignment {
+                        horizontal: HorizontalAlign::Center,
+                        ..Default::default()
+                    },
+                ),
+                ..Default::default()
+            });
 
         // Left Paddle
 
@@ -40,7 +57,7 @@ fn setup(
         commands.spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(1.0, 0.5, 0.5).into()),
             transform: Transform::from_xyz(0.0, 0.0, 1.0),
-            sprite: Sprite::new(Vec2::new(30.0, 30.0)),
+            sprite: Sprite::new(Vec2::new(15.0, 15.0)),
             ..Default::default()
         });
 }
