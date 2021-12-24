d2a3a909d688d6e4aa2344cb8ee51697df3e9ce9
miner0017 <miner.wk51@gmail.com>
2021-12-24 15:43:48 -0600

    Create our 2D camera Created our 2D text "Hello, World" using our downloaded font

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,21 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    println!("Hello, World!");
+        // 2d camera
+        commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+        commands.spawn_bundle(Text2dBundle {
+            text: Text::with_section(
+                "Hello, World!",
+                TextStyle {
+                    font: asset_server.load("fonts/font.ttf"),
+                    font_size: 60.0,
+                    color: Color::WHITE,
+                },
+                TextAlignment {
+                    vertical: VerticalAlign::Center,
+                    horizontal: HorizontalAlign::Center,
+                },
+            ),
+            ..Default::default()
+        });
 }
